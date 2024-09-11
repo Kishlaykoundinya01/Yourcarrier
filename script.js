d28199d6737eda9773446ec9918e9471938e886d
Kishlaykoundinya01 <147274329+Kishlaykoundinya01@users.noreply.github.com>
2024-09-11 23:32:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple form submission handler (can be enhanced later)
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting us. We will get back to you soon!');
+});
